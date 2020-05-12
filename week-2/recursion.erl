dd11331406673323f8b3a8b23c5bdeb1c8a71029
Mert Torun <mert@merttorun.com>
2020-05-12 12:16:19 +0300

    Hyper-dimensional cuts

--- week-2/recursion.erl
+++ week-2/recursion.erl
@@ -1,15 +1,20 @@
 -module(recursion).
 
--export([fibonacci/1, pieces/1, test/0]).
+-export([fibonacci/1, hyper_pieces/2, pieces/1,
+	 test/0]).
 
 fibonacci(0) -> 1;
 fibonacci(1) -> 1;
 fibonacci(N) when N > 1 ->
     fibonacci(N - 1) + fibonacci(N - 2).
 
-pieces(0) -> 1;
-pieces(1) -> 2;
-pieces(N) when N > 1 -> N + pieces(N - 1).
+pieces(N) -> hyper_pieces(2, N).
+
+%% D: number of dimensions, N: number of cuts
+hyper_pieces(_, 0) -> 1;
+hyper_pieces(1, N) when N > 0 -> N + 1;
+hyper_pieces(D, N) when (D > 1) and (N > 0) ->
+    hyper_pieces(D, N - 1) + hyper_pieces(D - 1, N - 1).
 
 test_fibonacci() ->
     {'EXIT', {function_clause, _}} = (catch fibonacci(-3)),
@@ -28,4 +33,15 @@ test_pieces() ->
     16 = pieces(5),
     ok.
 
-test() -> ok = test_fibonacci(), ok = test_pieces(), ok.
+test_hyper_pieces() ->
+    5 = hyper_pieces(1, 4),
+    11 = hyper_pieces(2, 4),
+    15 = hyper_pieces(3, 4),
+    16 = hyper_pieces(4, 4),
+    ok.
+
+test() ->
+    ok = test_fibonacci(),
+    ok = test_pieces(),
+    ok = test_hyper_pieces(),
+    ok.
