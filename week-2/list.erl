a7b1b1d1660e9f7734ce2ff0c994a2457129cfa5
Mert Torun <mert@merttorun.com>
2020-05-25 14:18:40 +0300

    Define list:take/2

--- week-2/list.erl
+++ week-2/list.erl
@@ -1,6 +1,7 @@
 -module(list).
 
--export([double/1, evens/1, max/1, product/1, test/0]).
+-export([double/1, evens/1, max/1, product/1, take/2,
+	 test/0]).
 
 % when you have a linked list that needs to be folded/reduced to
 % the same type as its contents with a commutative operation,
@@ -20,6 +21,10 @@ double(Ns) -> [2 * N || N <- Ns].
 
 evens(Ns) -> [N || N <- Ns, N rem 2 == 0].
 
+take(0, _) -> [];
+take(_, []) -> [];
+take(N, [X | Xs]) -> [X | take(N - 1, Xs)].
+
 test_product() ->
     6 = product([1, 2, 3]), 28 = product([2, 7, 2]), ok.
 
@@ -34,9 +39,17 @@ test_double() ->
 test_evens() ->
     [2] = evens([1, 2, 3]), [2, 8] = evens([2, 7, 8]), ok.
 
+test_take() ->
+    [] = take(0, "hello"),
+    "hell" = take(4, "hello"),
+    "hello" = take(5, "hello"),
+    "hello" = take(9, "hello"),
+    ok.
+
 test() ->
     ok = test_product(),
     ok = test_max(),
     ok = test_double(),
     ok = test_evens(),
+    ok = test_take(),
     ok.
