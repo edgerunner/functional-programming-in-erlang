d0a4d3cc8d3d7e1f8ad5edfe00851a5c74b5bfdf
Mert Torun <mert@merttorun.com>
2020-05-25 14:40:21 +0300

    Define list:nub/1

--- week-2/list.erl
+++ week-2/list.erl
@@ -1,7 +1,7 @@
 -module(list).
 
--export([double/1, evens/1, max/1, product/1, take/2,
-	 test/0]).
+-export([double/1, evens/1, max/1, nub/1, product/1,
+	 take/2, test/0]).
 
 % when you have a linked list that needs to be folded/reduced to
 % the same type as its contents with a commutative operation,
@@ -25,6 +25,17 @@ take(0, _) -> [];
 take(_, []) -> [];
 take(N, [X | Xs]) -> [X | take(N - 1, Xs)].
 
+nub(L) -> nub(L, []).
+
+nub([], Nubs) -> lists:reverse(Nubs);
+nub([X | Xs], Nubs) ->
+    case lists:member(X, Nubs) of
+      true -> nub(Xs, Nubs);
+      false -> nub(Xs, [X | Nubs])
+    end.
+
+% TESTS
+
 test_product() ->
     6 = product([1, 2, 3]), 28 = product([2, 7, 2]), ok.
 
@@ -46,10 +57,18 @@ test_take() ->
     "hello" = take(9, "hello"),
     ok.
 
+test_nub() ->
+    [] = nub([]),
+    [3, 2, 7] = nub([3, 2, 7]),
+    [5] = nub([5, 5, 5]),
+    [2, 4, 1, 3] = nub([2, 4, 1, 3, 3, 1]),
+    ok.
+
 test() ->
     ok = test_product(),
     ok = test_max(),
     ok = test_double(),
     ok = test_evens(),
     ok = test_take(),
+    ok = test_nub(),
     ok.
